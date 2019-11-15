91f3eb77ea3c6303fd0b591f62c2fef1764ff418
leeu3581 <urilee@Uris-MacBook.local>
2019-11-15 14:51:54 +0000

    header and footer- not much there at the moment

--- private/shared/header.php
+++ private/shared/header.php
@@ -1,2 +1,18 @@
 // File used as top of all webpages
 // Mostly html code
+
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>Welcome to KCLSU</title>
+    <meta charset="utf-8">
+   <!-- needs link to css -->
+  </head>
+
+  <body>
+    <header>
+      <h1>KCLSU</h1>
+    </header>
+<!-- needs navigation links  -->
+
