9b1a1a5baa650a3be25b021a11da69beb8877d57
Harald Gerhardsen <k1889292@kcl.ac.uk>
2019-11-16 14:40:18 +0000

    Add initialise.php
    Add a first version of initialise.php. Includes commented blocks of code for future files (include statements and method calls, etc). The file initialise.php is used in the beginning of all webpages.

--- private/initialise.php
+++ private/initialise.php
@@ -1,1 +1,23 @@
-// Code required to initialise any webpage 
+<?php 
+/* 
+Code required to initialise any webpage.
+*/
+
+
+ob_start(); // output buffering
+
+define("PRIVATE_PATH", dirname(__FILE__));
+define("PROJECT_PATH", dirname(PRIVATE_PATH));
+define("PUBLIC_PATH", PROJECT_PATH . '/public');
+define("SHARED_PATH", PRIVATE_PATH . '/shared');
+define("CLASSES_PATH", SHARED_PATH . '/classes');
+
+//require_once('functions.php');
+//require_once('database.php');
+//require_once('query_functions.php');
+//require_once('validation_functions.php');
+
+// $db = db_connect();
+$errors = [];
+
+?>
