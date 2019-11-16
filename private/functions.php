9a4dde3e089a8ff591d88c2bfedfde50446d4aa4
Harald Gerhardsen <k1889292@kcl.ac.uk>
2019-11-16 15:08:12 +0000

    Add functions.php
    Create file functions.php. This file contains functions that are used through out the project.

--- private/functions.php
+++ private/functions.php
@@ -1,1 +1,39 @@
+<?php 
 // Common functions used throughout project
+
+/* Dynamically find a url */
+function url_for($script_path){
+	if($script_path[0] != '/'){
+		$script_path = "/" . $script_path;
+	}
+	return WWW_ROOT . $script_path;
+}
+
+/* URL escaping */
+function u($string=""){
+	return urlencode($string);
+}
+
+/* HTML escaping */
+function h($string=""){
+	return htmlspecialchars($string);
+}
+
+/* Redirects to a given location */
+function redirect_to($location){
+	header("Location: " . $location);
+	exit;
+}
+
+/* Checks if web page loaded through a post request */
+function is_post_request(){
+	return $_SERVER['REQUEST_METHOD'] == 'POST';
+}
+
+/*Checks whether web page loaded through get request */
+function is_get_request(){
+	return $_SERVER['REQUEST_METHOD'] == 'GET';
+}
+
+
+?>
