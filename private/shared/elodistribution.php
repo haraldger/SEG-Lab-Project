a926995a3f3df1e0e22d90bdb0b6317d96c10126
leeu3581 <urilee@Uris-MacBook.local>
2019-11-19 17:02:59 +0000

    distribution without real data

--- private/shared/elodistribution.php
+++ private/shared/elodistribution.php
@@ -10,19 +10,30 @@
    $eloRatings["person8"]= 1585;
    $eloRatings["person9"]= 1521;
    $eloRatings["person10"]= 1447;
-   $eloRatings["person11"]= 1950;
-   $eloRatings["person12"]= 1800;
+   $eloRatings["person11"]= 1700;
+   $eloRatings["person12"]= 1500;
+   $eloRatings["person13"]= 1521;
+   $eloRatings["person14"]= 1234;
+   $eloRatings["person15"]= 1700;
+   $eloRatings["person16"]= 1342;
+   $eloRatings["person17"]= 1540;
+   $eloRatings["person18"]= 1244;
+   $eloRatings["person19"]= 1320;
+   $eloRatings["person20"]= 1490;
+   $eloRatings["person21"]= 2030;
 
    $distribution = array();
    //ranges for distribution
    //assumption:
-   //main chunck of elo values lay in 1000-2000
+   //main chunck of elo values lay in 1000-2000 (Source: Wiki and online)
     for($j = 1000; $j<2100; $j+=100){
         $distribution[$j] = 0;
     }
     $distribution[2100] = 0;
     //assume 2100 bracket is anything above 2000 elo rating (rare)
 
+    //seperate all Elo ratings into 1000's range
+    //Eg. 1000-2000, 2000-3000.... 
    foreach($eloRatings as $person => $score){
     switch ($score) {
         case $score <= 1000:
@@ -63,88 +74,93 @@
             break;                                   
     }
    }
-
+   //height and width of the pane of the graph
    $width = 600;
    $height = 300;
-
+   //padding to create axis inside pane
    $padding = 30;
-
+   //axis height
    $axisHeight = $height - ($padding * 2); //290
    $axisWidth = $width - ($padding * 2); //590
 
+   //colours and image set up
    $img = imagecreatetruecolor($width, $height);
    $black = imagecolorallocate($img, 0, 0, 0);
    $white = imagecolorallocate($img, 255, 255, 255);
    $red = imagecolorallocate($img, 255, 153, 153);
    $gray_dark = imagecolorallocate($img,0x7f,0x7f,0x7f);
+   $nice_blue = imagecolorallocate($img,0,206,209);
+   $almost_black_blue = imagecolorallocate($img,0,0,51);
+   $white_pink = imagecolorallocate($img,255,102,178);
 
    //background setup
    imagefill($img, 0, 0, $white);
    imagefilledrectangle($img,0,0,$width,$height,white);
 
+   //find the max rating (to set as max height of the graph)
    $maxRating = 0;
-
    foreach($distribution as $key => $value){
        $maxRating = max($maxRating, $value);
    }
-
+   //numCols is the number of colums in distribution
    $numCols = count($distribution);
     
    $spaceBetweenCols = 30;
-    $colWidth = ($axisWidth / $numCols)- $spaceBetweenCols;
+   $colWidth = ($axisWidth / $numCols)- $spaceBetweenCols;
    
+   //creating the bars and the numbering
    $i=0;
-   
    foreach($distribution as $key => $value){
     $column_height =  $axisHeight * ($value/ $maxRating)- ($padding);
     $x1 = ($i* $spaceBetweenCols) + ($i* $colWidth) + $padding;
     $y1 = $height -$padding;
     $x2 = $x1 + $colWidth;
     $y2 = $axisHeight-$column_height;
-
-    imagerectangle($img, $x1, $y1, $x2, $y2, $black);
+    //bar
+    imagefilledrectangle($img, $x1, $y1, $x2, $y2, $nice_blue);
+    //bar lines and labels
+    $centreBarX = $x1 + ($x2-$x1)/2 - 1;
+    $centreBarY = $y1 + ($y2-$y1)/2;
+    imagestring ($img, 2,  $x1, $y1, $key, $gray_dark);
+    imagestring ($img, 2,  $centreBarX, $centreBarY, $value, $white_pink);
+    imageline($img, $padding, $y2, $padding + 5,  $y2, $gray_dark);
     $i++;
    }
-
+   //padding and axis variables
    $minX = $padding;
    $minY = $padding;
    $maxY = $height - $padding;
    $maxX = $width - $padding;
 
-   //x and y axis
+    //x and y axis
     imageline($img, $minX, $minY, $minX, $maxY, $gray_dark);
     imageline($img, $minX, $maxY, $maxX, $maxY, $gray_dark);
 
-    //horixontal grids
+    //horixontal lines on x axis
     for($i =$maxY; $i>$minY; $i--){
-        if($i%100 ==0){
+        if($i%50 ==0){
             imageline($img, $minX, $i, $maxX, $i, $gray_dark);
             //label
-
         }
-        else if ($i%50 ==0){
-            imageline($img, $minX, $i, $minX + 5, $i, $gray_dark);
+        else if ($i%25 ==0){
+            // imageline($img, $minX, $i, $minX + 5, $i, $gray_dark);
         }
         else{
 
         }
     }
 
-
-    //headers
-    //Graph Title
+    //headers and graph titles
     $titleX = $width /2 - 50;
     $titleY = $padding /2 -10;
-    imagestring ($img, 5, $titleX , $titleY , "Members Elo Distribution" , $gray_dark); 
-    imagestring ($img, 2,  $titleX, ($height - ($padding/2)) , "Elo Ratings" , $gray_dark);
-    imagestringup($img, 2, ($padding/2) , ($height/2) , "Frequency" , $gray_dark);  
+    //the title below is commented out because gdf font is horrible
+    // imagestring ($img, 5, ($titleX-15) , $titleY , "Members Elo Distribution" , $almost_black_blue); 
+    imagestring ($img, 2,  ($titleX+25), ($height - ($padding/2)) , "Elo Ratings" , $gray_dark);
+    imagestringup($img, 2, 5 , ($height/2) , "Frequency" , $gray_dark);  
 
     header('Content-Type: image/png');
     imagepng($img);
     imagedestroy($img);
-    
-    
-
 ?>
 
 
