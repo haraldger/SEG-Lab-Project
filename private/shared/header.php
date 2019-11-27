57a69d7411d92b0ee7ef75613588aeb226365dd3
Uri Lee <shlee9817@gmail.com>
2019-11-27 00:25:59 +0000

    adding content and v basic styling

--- private/shared/header.php
+++ private/shared/header.php
@@ -1,5 +1,5 @@
-// File used as top of all webpages
-// Mostly html code
+<!-- // File used as top of all webpages
+// Mostly html code -->
 
 <!doctype html>
 
@@ -7,12 +7,38 @@
   <head>
     <title>Welcome to KCLSU</title>
     <meta charset="utf-8">
+    <!-- bootstrap link to be uncommented  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
    <!-- needs link to css -->
   </head>
 
   <body>
     <header>
-      <h1>KCLSU</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">KCLSU</a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+    <li class="nav-item active">
+    <a class="nav-link" href="../public/index.php">Home<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+    <a class="nav-link" href="../public/about.php">About</a>
+      </li>
+      <li class="nav-item active">
+    <a class="nav-link" href="../public/contact.php">Contact</a>
+      </li>
+        
+        </ul>
+        <span class="navbar-text">
+        <button type="button" class="btn btn-primary btn-lg">Login</button>
+        <button type="button" class="btn btn-secondary btn-lg">Register</button>
+        
+    </span>
+        </div>
+      </nav>
     </header>
 <!-- needs navigation links  -->
 
