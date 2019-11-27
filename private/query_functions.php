9c3d38664081ffd8b7b593e8d081fe643388a9f0
RaviGhaghada <ravighaghada28@gmail.com>
2019-11-27 23:03:46 +0000

    Removed all search-related sql functions

--- private/query_functions.php
+++ private/query_functions.php
@@ -1,13 +1,5 @@
 <?php
 
-  // Members
 
-  function find_all_members() {
-    global $db;
-    $sql = "SELECT * FROM members ";
-    $result = mysqli_query($db, $sql);
-    confirm_result_set($result);
-    return $result;
-  }
 
 ?>
