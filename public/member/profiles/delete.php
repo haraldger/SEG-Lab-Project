b5ab8604dc3ad87f7d6d32336423add585166733
KristinBenson <kbenson811@gmail.com>
2019-12-04 00:38:22 +0000

    refactor delete to be OO

--- public/member/profiles/delete.php
+++ public/member/profiles/delete.php
@@ -5,14 +5,18 @@ if(!isset($_GET['id'])) {
   redirect_to(url_for('/member/profiles/index.php'));
 }
 $id = $_GET['id'];
+$member = Member::find_by_id($id);
+if($member == false) {
+  redirect_to(url_for('/member/profiles/index.php'));
+}
 
 if(is_post_request()) {
-
-    $result = delete_member($id);
+    $result = $member->delete();
+    //$session->message('The member was deleted successfully.');
     redirect_to(url_for('/member/profiles/index.php'));
   
   } else {
-    $member = find_member_by_id($id);
+    // Display form
   }
   
   ?>
@@ -22,13 +26,13 @@ if(is_post_request()) {
 
 <div id="content">
 
-  <a class="back-link" href="<?php echo url_for('/member/profiles/index.php?id='.$id);  ?>">&laquo; Back to Profile</a>
+  <a class="back-link" href="<?php echo url_for('/member/profiles/index.php?id='. h(u($id)));  ?>">&laquo; Back to Profile</a>
 
   <div class="member delete">
     <h1>Delete Profile</h1>
     <p>Are you sure you want to delete your profile?</p>
 
-    <form action="<?php echo url_for('/member/profiles/delete.php?id=' . h(u($member['memberID']))); ?>" method="post">
+    <form action="<?php echo url_for('/member/profiles/delete.php?id=' . h(u($member->id))); ?>" method="post">
       <div id="operations">
         <input type="submit" name="commit" value="Delete" />
       </div>
