e62503612b6675a22631233c2cd6e839eaa92985
KristinBenson <kbenson811@gmail.com>
2019-12-04 00:38:03 +0000

    refactor edit page to be OO

--- public/member/profiles/edit.php
+++ public/member/profiles/edit.php
@@ -2,34 +2,33 @@
 
 <?php
     if(!isset($_GET['id'])) {
-        redirect_to(url_for('/member/profiles/index.php'));
+        redirect_to(url_for('/member/profiles/index.php')); //replace with home page?
       }
     $id = $_GET['id'];
+    $member = Member::find_by_id($id);
+    if($member == false) {
+      redirect_to(url_for('//member/profiles/index.php'));
+    }
     
     if(is_post_request()) {
-    
-        $member = [];
-        $member['id'] = $id;
-        $member['fName'] = $_POST['fName'] ?? '';
-        $member['lName'] = $_POST['lName'] ?? '';
-        $member['address'] = $_POST['address'] ?? '';
-        $member['phoneNum'] = $_POST['phoneNum'] ?? '';
-        $member['gender'] = $_POST['gender'] ?? '';
-        $member['dob'] = $_POST['dob'] ?? '';
-    
-        $result = update_member($member);
+        $args = $_POST['member'];
+        $member->merge_attributes($args);
+        $result = $member->save();
+        echo $result;
+
         if($result === true) {
-        redirect_to(url_for('/member/profiles/index.php?id=' . $id));
+          //$session->message('The member was updated successfully.');
+          redirect_to(url_for('/member/profiles/index.php?id=' . $id));
         } else {
-        $errors = $result;
+          // show errors
         }
     
     } else {  
-        $member = find_member_by_id($id);
+        // display the form
     }
 ?>
 
-<?php $page_title = 'Edit Subject'; ?>
+<?php $page_title = 'Edit Member'; ?>
 <?php include(SHARED_PATH . '/header.php'); ?>
 
 <div id="content">
@@ -39,29 +38,35 @@
   <div class="member edit">
     <h1>Edit Profile</h1>
 
-    <?php echo display_errors($errors); ?>
+    <?php echo display_errors($member->errors); ?>
 
     <form action="<?php echo url_for('/member/profiles/edit.php?id=' . h(u($id))); ?>" method="post">
-        First Name:<br>
-        <input type="text" name="fName" value="<?php echo h($member['fName']); ?>" /><br><br>
-        Last Name:<br>
-        <input type="text" name="lName" value="<?php echo h($member['lName']); ?>" /><br><br>
-        Address:<br>
-        <input type="text" name="address" value="<?php echo h($member['address']); ?>" /><br><br>
-        Phone Number:<br>
-        <input type="text" name="phoneNum" value="<?php echo h($member['phoneNum']); ?>" /><br><br>
-        Gender:<br>
-        <input type="radio" name="gender" value="Male" <?php if ($member['gender']=='Male') { echo 'checked'; } ?>> Male<br>
-        <input type="radio" name="gender" value="Female" <?php if ($member['gender']=='Female') { echo 'checked'; } ?>> Female<br>
-        <input type="radio" name="gender" value="Other" <?php if ($member['gender']=='Other') { echo 'checked'; } ?>> Other<br><br>
-        Date of Birth:<br>
-        <input type="date" name="dob" value="<?php echo h($member['dob']); ?>" /><br><br>
+        
+      <?php
+      if(!isset($member)) {
+        redirect_to(url_for('/member/profiles/index.php')); //change to home page
+      }
+      ?>
+
+      First Name:<br>
+      <input type="text" name="member[first_name]" value="<?php echo h($member->first_name); ?>" /><br><br>
+      Last Name:<br>
+      <input type="text" name="member[last_name]" value="<?php echo h($member->last_name); ?>" /><br><br>
+      Address:<br>
+      <input type="text" name="member[address]" value="<?php echo h($member->address); ?>" /><br><br>
+      Phone Number:<br>
+      <input type="text" name="member[phone_number]" value="<?php echo h($member->phone_number); ?>" /><br><br>
+      Gender:<br>
+      <input type="radio" name="member[gender]" value="Male" <?php if ($member->gender=='Male') { echo 'checked'; } ?>> Male<br>
+      <input type="radio" name="member[gender]" value="Female" <?php if ($member->gender=='Female') { echo 'checked'; } ?>> Female<br>
+      <input type="radio" name="member[gender]" value="Other" <?php if ($member->gender=='Other') { echo 'checked'; } ?>> Other<br><br>
+      Date of Birth:<br>
+      <input type="date" name="member[date_of_birth]" value="<?php echo h($member->date_of_birth); ?>" /><br><br>
+  
         <div id="operations">
             <input type="submit" value="Submit" />
         </div><br>
     </form>
-
-
 
   </div>
 
