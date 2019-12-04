725c476b75761545c41142393919189555ee1105
KristinBenson <kbenson811@gmail.com>
2019-12-04 00:35:40 +0000

    add in require statements for member class and make db connect OOP

--- private/initialise.php
+++ private/initialise.php
@@ -29,8 +29,10 @@ require_once('database.php');
 require_once('query_functions.php');
 require_once('validation_functions.php');
 
+require_once('shared/classes/databaseobject.class.php');
+require_once('shared/classes/member.class.php');
 
-$db = db_connect();
-$errors = [];
+$database = db_connect();
+DatabaseObject::set_database($database);
 
 ?>
