1a6f73e8ad26e5ecfb27f3acc75951bb1c6e6c6d
KristinBenson <kbenson811@gmail.com>
2019-12-07 18:17:01 +0000

    add functions to create matches with given id array

--- private/functions.php
+++ private/functions.php
@@ -120,4 +120,42 @@ function display_errors($errors=array()) {
   return $output;
 }
 
+function generate_first_round($participant_ids) {
+  // LIMIT NUM. OF PARTICIPANTS TO BETWEEN 4 AND 32
+
+  // calculate number of bye matches
+  $num_participants = count($participant_ids);
+  $num_byes = 0;
+
+  if ($num_participants < 32) {
+    if ($num_participants > 16) $num_byes = 32 - $num_participants;
+    else {
+      if ($num_participants > 8) $num_byes = 16 - $num_participants;
+      else {
+        if ($num_participants > 4) $num_byes = 8 - $num_participants;
+      }
+    }
+  }
+
+  // randomize participant list
+  shuffle($participant_ids);
+
+  // add byes to participant list
+  $array_index = 0;
+  for ($i = 0; $i < $num_byes; $i++) {
+    array_splice($participant_ids, $array_index, 0, "-1");
+    $array_index += 2; 
+  }
+
+  return generate_next_round($participant_ids);
+}
+
+function generate_next_round($participant_ids) {
+  $match_ids = array();
+  for ($i = 0; $i < count($participant_ids); $i+=2){
+    array_push($match_ids, array("competitorID1" => $participant_ids[$i], "competitorID2" => $participant_ids[$i+1]));
+  }
+  return $match_ids;
+}
+
 ?>
