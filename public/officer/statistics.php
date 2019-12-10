5351cf4d97eb86187b99f4e45e732e7b8905503c
Uri Lee <shlee9817@gmail.com>
2019-12-10 16:12:53 +0000

    elo distribution and View Members

---
+++ public/officer/statistics.php
@@ -0,0 +1,18 @@
+<?php require_once('../../private/initialise.php'); 
+if(!(am_sysadmin() || am_officer())){
+	redirect_to(url_for('../public'));
+}
+?>
+
+
+<?php include(SHARED_PATH . '/officer_header.php'); ?>
+
+<div id="content" class="container mt-5 mb-5">
+  <div id="main-menu">
+    <h2>Elo statistics</h2>
+    <img src="../../private/shared/elodistribution.php">
+  </div>
+
+</div>
+
+<?php include(SHARED_PATH . '/footer.php'); ?>
--- public/officer/viewMembers.php
+++ public/officer/viewMembers.php
@@ -1,12 +1,19 @@
 <?php require_once('../../private/initialise.php'); 
 
 include(SHARED_PATH . '/officer_header.php');
-//include(SHARED_PATH . '/classes/member.class.php');
+
+$sys_admin = am_sysadmin();
 ?>
 
 <div class="container mt-5 mb-5">
-    <h1>View Members</h1><br><br>
-    
+    <?php 
+        if($sys_admin){  
+    ?>
+        <h1>View Members</h1><br><br>
+    <?php  } else {?>
+        <h1>View Members & Officers</h1><br><br>
+     <?php }?>
+
     <table class="table">
     <thead>
         <tr>
@@ -14,7 +21,8 @@ include(SHARED_PATH . '/officer_header.php');
             <th scope="col">Name</th>
             <th scope="col">Rating</th>     
 			<th scope="col">Email</th>
-			<th scope="col">&nbsp;</th>			
+			<th scope="col">&nbsp;</th>	
+            <?php if($sys_admin){ ?><th scope="col">&nbsp;</th>	<?php }?>	
         </tr>
     </thead>
         <?php
@@ -27,6 +35,20 @@ include(SHARED_PATH . '/officer_header.php');
                     echo "<td>$member->rating</td>";
                     echo "<td>$member->email</td>";
                     echo "<td> <a href=../member/profiles/index.php?id=$member->id>View Profile</td>";
+                    if($sys_admin){
+                        echo "<td>"; 
+                        if($member->role == "Member"){
+                            
+                            echo "<a href=roleEdit.php?id=$member->id>Promote";
+                        }
+                        else if($member->role == "Officer"){
+                            echo "<a href=roleEdit.php?id=$member->id>Demote";
+                        }
+                        else {
+                            echo "System Admin";
+                        }
+                        echo "</td>";	
+                    }
                 echo "</tr>";
             }
         ?>
