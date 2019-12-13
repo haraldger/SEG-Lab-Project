3ccce04da3137433465e712fb6bdd1f2aff33082
Uri Lee <shlee9817@gmail.com>
2019-12-13 18:32:06 +0000

    MemberDelete clean

--- public/member/profiles/delete.php
+++ public/member/profiles/delete.php
@@ -1,28 +1,22 @@
 <?php
 require_once('../../../private/initialise.php');
 
-if(!isset($_GET['id'])) {
-  redirect_to(url_for('/member/profiles/index.php'));
-}
+if(!isset($_GET['id'])) {redirect_to(url_for('/member/profiles/index.php'));}
+
 $id = $_GET['id'];
 $member = Member::find_by_id($id);
-if($member == false) {
-  redirect_to(url_for('/member/profiles/index.php'));
-}
 
-if(get_session_id()!=$id){
-	redirect_to(url_for('/index.php'));
-}
+// redirects
+if($member == false) {redirect_to(url_for('/member/profiles/index.php'));}
+if(get_session_id()!=$id){redirect_to(url_for('/index.php'));}
 
 if(is_post_request()) {
     $result = $member->delete();
-    //$session->message('The member was deleted successfully.');
     redirect_to(url_for('/logout.php'));
 	
   } else {
     // Display form
   }
-  
   ?>
 
 <?php $page_title = 'Delete Profile'; ?>
@@ -31,7 +25,7 @@ if(is_post_request()) {
 <div id="content" class="container mt-5 mb-5">
 
   <a class="back-link" href="<?php echo url_for('/member/profiles/index.php?id='. h(u($id)));  ?>">&laquo; Back to Profile</a>
-<br><br>
+  <br><br>
   <div class="member delete">
     <h1>Delete Profile</h1><br>
     <p>Are you sure you want to delete your profile?</p><br>
@@ -42,7 +36,6 @@ if(is_post_request()) {
       </div>
     </form>
   </div>
-
 </div>
 
 <?php include(SHARED_PATH . '/footer.php'); ?>
